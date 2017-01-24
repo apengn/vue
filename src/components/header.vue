4d123242fc77810f8974964d42076699643c1e2f
zhaoheng <hueguan@163.com>
2017-01-24 15:44:19 +0800

    增加文件

---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <div>我是header</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
